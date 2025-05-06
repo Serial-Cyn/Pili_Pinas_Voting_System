c835b2c61e82655af12886a638041f76ecb82f58
Darkhious <162713985+Darkhious@users.noreply.github.com>
2025-05-07 02:43:34 +0800

    Add files via upload

---
+++ VotingSystemV2/src/security/Block.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package security;
+
+/**
+ *
+ * @author Christian
+ */
+
+import java.security.MessageDigest;
+import java.util.Date;
+
+public class Block {
+    public String hash;
+    public String previousHash;
+    private String data; // vote data
+    private long timeStamp;
+
+    public Block(String data, String previousHash) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+        this.hash = calculateHash();
+    }
+
+    public String calculateHash() {
+        try {
+            String input = previousHash + Long.toString(timeStamp) + data;
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = digest.digest(input.getBytes("UTF-8"));
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hashBytes) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public String getPreviousHash() {
+        return previousHash;
+    }
+}
---
+++ VotingSystemV2/src/voters/Variables.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package voters;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Christian
+ */
+public class Variables {
+    public static boolean isVoting = false;
+    
+    protected static String PresID = "";
+    protected static String PresName = "";
+    protected static String PresPartylist = "";
+    
+    protected static String VPresID = "";
+    protected static String VPresName = "";
+    protected static String VPresPartylist = "";
+    
+    public static ArrayList<String> SelectedSenators = new ArrayList<>();
+}
